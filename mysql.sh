bde636bb7eb6ce9f2b77a4fce27dec6ea064f841
iamreymond <emon.aprigel@yahoo.com>
2024-06-01 01:55:18 +0800

    qwe

--- mysql.sh
+++ mysql.sh
@@ -1,34 +1,61 @@
 #!/bin/bash
 
-sudo -i
+# Update OS with latest patches
 yum update -y
-yum install mariadb-server -y
 
+# Set Repository
+yum install epel-release -y
+
+# Install MariaDB Package
+yum install git mariadb-server -y
+
+# Starting & enabling mariadb-server
 systemctl start mariadb
 systemctl enable mariadb
 
-mysql -u root -padmin123 <<EOF
-create database accounts;
-grant all privileges on accounts.* TO 'admin'@'%' identified by 'admin123';
-FLUSH PRIVILEGES;
-exit;
+# Run mysql secure installation script
+# Automating mysql_secure_installation using a here document
+mysql_secure_installation <<EOF
+
+Y
+admin123
+admin123
+Y
+n
+Y
+Y
 EOF
 
+# Set DB name and users
+mysql -u root -padmin123 <<MYSQL_SCRIPT
+CREATE DATABASE accounts;
+GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY 'admin123';
+FLUSH PRIVILEGES;
+EXIT;
+MYSQL_SCRIPT
+
+# Download Source code & Initialize Database
+git clone -b main https://github.com/hkhcoder/vprofile-project.git
+cd vprofile-project
 mysql -u root -padmin123 accounts < src/main/resources/db_backup.sql
 
-mysql -u root -padmin123 accounts <<EOF
-show tables;
-exit;
-EOF
+# Verify tables
+mysql -u root -padmin123 accounts <<MYSQL_VERIFY
+SHOW TABLES;
+EXIT;
+MYSQL_VERIFY
 
+# Restart mariadb-server
 systemctl restart mariadb
 
+# Starting the firewall and allowing the mariadb to access from port no. 3306
 systemctl start firewalld
 systemctl enable firewalld
-
 firewall-cmd --get-active-zones
 firewall-cmd --zone=public --add-port=3306/tcp --permanent
 firewall-cmd --reload
 
+# Restart mariadb-server again to ensure all changes take effect
 systemctl restart mariadb
 
+echo "MariaDB setup and configuration completed successfully."
