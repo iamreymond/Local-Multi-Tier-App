39ea83b47f794b50d670a4fc2c621e5e3048ac91
iamreymond <emon.aprigel@yahoo.com>
2024-05-31 14:43:00 +0800

    apply changes

--- .vagrant/bundler/global.sol
+++ .vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]]],"checksum":"c410c8cd7720abe828ac75441a4e7ab465ad362de0743c1e7df4d6f147aef5f0","vagrant_version":"2.4.1"}
+{"dependencies":[["vagrant-hostmanager",["= 1.8.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"36c5cda303ef35fb837811f94a090d44cb2affbe62a7e1f4c4bc89c62a1cd413","vagrant_version":"2.4.1"}
--- memcached.sh
+++ memcached.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "CHANGE USER TO ROOT"
 sudo -i
 
+echo "YUM UPDATE"
 yum update -y
 yum install memcached -y
 
+echo "SYSTEMCTL START MEMCAHCED"
 systemctl start memcached
 systemctl enable memcached
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/sysconfig/memcached
 systemctl restart memcached
 
+echo "SYSTEMCTL START FIREWALLD"
 systemctl start firewalld
 systemctl enable firewalld
 firewall-cmd --add-port=11211/tcp
@@ -17,4 +21,6 @@ firewall-cmd --runtime-to-permanent
 firewall-cmd --add-port=11111/udp
 firewall-cmd --runtime-to-permanent
 memcached -p 11211 -U 11111 -u memcached -d
+
+echo "FINISHED SCRIPTS"
 
